6a1638a059279aa87b734ed54ffc2f06a9bc7a9e
ALUMNADO\humiva <alackbarsenpai@gmail.com>
2024-05-14 16:38:02 +0200

    base de datos corregida

--- db/db.sql
+++ db/db.sql
@@ -1,38 +1,39 @@
-CREATE DATABASE if NOT exists pet4sitter; 
+CREATE DATABASE IF NOT EXISTS pet4sitter CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 USE pet4sitter;
 
-CREATE TABLE IF NOT EXISTS users(
-id_user INT AUTO_INCREMENT,
-nàme VARCHAR(30) NOT NULL, 
-surname VARCHAR(60),
-email VARCHAR(60) NOT NULL, 
-dni VARCHAR(9),
-pàssword VARCHAR(30) NOT NULL,
-maxpets INT,
-price DOUBLE,
-location VARCHAR(50),
-premium TINYINT,
-sitter TINYINT,
-PRIMARY KEY (id)
-);
-CREATE TABLE if NOT EXISTS reviews(
-id_review INT AUTO_INCREMENT PRIMARY KEY,
-valoration INT NOT NULL, 
-id_reviewed INT NOT NULL,
-id_reviewer INT NOT NULL
-);
+CREATE TABLE IF NOT EXISTS users (
+    id_user INT AUTO_INCREMENT,
+    name VARCHAR(30) CHARACTER SET utf8mb4 NOT NULL,
+    surname VARCHAR(60) CHARACTER SET utf8mb4,
+    email VARCHAR(60) CHARACTER SET utf8mb4 NOT NULL,
+    dni VARCHAR(9) CHARACTER SET utf8mb4,
+    password VARCHAR(30) CHARACTER SET utf8mb4 NOT NULL,
+    maxpets INT,
+    price DOUBLE,
+    location VARCHAR(50) CHARACTER SET utf8mb4,
+    premium TINYINT,
+    sitter TINYINT,
+    PRIMARY KEY (id_user)
+) CHARACTER SET utf8mb4;
 
-CREATE TABLE if NOT EXISTS products (
-id_product INT AUTO_INCREMENT PRIMARY KEY,
-price DOUBLE NOT NULL,
-quantity INT NOT NULL,   
-dèscription VARCHAR(99)
-);
+CREATE TABLE IF NOT EXISTS reviews (
+    id_review INT AUTO_INCREMENT PRIMARY KEY,
+    valuation INT NOT NULL,
+    id_reviewed INT NOT NULL,
+    id_reviewer INT NOT NULL
+) CHARACTER SET utf8mb4;
 
-CREATE TABLE if NOT EXISTS chat(
-id_chat INT AUTO_INCREMENT PRIMARY KEY,
-id_reciever INT NOT NULL,
-id_sender INT NOT NULL,
-messages VARCHAR NOT NULL
-);
+CREATE TABLE IF NOT EXISTS products (
+    id_product INT AUTO_INCREMENT PRIMARY KEY,
+    price DOUBLE NOT NULL,
+    quantity INT NOT NULL,
+    description VARCHAR(99) CHARACTER SET utf8mb4
+) CHARACTER SET utf8mb4;
+
+CREATE TABLE IF NOT EXISTS chat (
+    id_chat INT AUTO_INCREMENT PRIMARY KEY,
+    id_receiver INT NOT NULL,
+    id_sender INT NOT NULL,
+    messages VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL
+) CHARACTER SET utf8mb4;
