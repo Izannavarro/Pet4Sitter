d16ad3b348256aec6fdb9946d64371bf9a18c47f
IZANNAVARROPC\izann <izannavarro2005@gmail.com>
2024-05-15 20:08:45 +0200

    Creando archivos de idiomas para frmConfiguracion

--- src/Forms/FrmConfiguracion.Designer.cs
+++ src/Forms/FrmConfiguracion.Designer.cs
@@ -29,16 +29,16 @@
         private void InitializeComponent()
         {
             this.lblConfiguracionAvanzada = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
+            this.lblIdioma = new System.Windows.Forms.Label();
+            this.lblTema = new System.Windows.Forms.Label();
+            this.lblPrivacidad = new System.Windows.Forms.Label();
+            this.lblEliminar = new System.Windows.Forms.Label();
+            this.lblDesactivar = new System.Windows.Forms.Label();
+            this.cmbIdioma = new System.Windows.Forms.ComboBox();
+            this.cmbTema = new System.Windows.Forms.ComboBox();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnDesactivar = new System.Windows.Forms.Button();
+            this.lbl2pasos = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblConfiguracionAvanzada
@@ -51,109 +51,108 @@
             this.lblConfiguracionAvanzada.TabIndex = 0;
             this.lblConfiguracionAvanzada.Text = "Configuracion Avanzada:";
             // 
-            // label2
+            // lblIdioma
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(149, 123);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(92, 29);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Idioma:";
+            this.lblIdioma.AutoSize = true;
+            this.lblIdioma.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIdioma.Location = new System.Drawing.Point(149, 123);
+            this.lblIdioma.Name = "lblIdioma";
+            this.lblIdioma.Size = new System.Drawing.Size(92, 29);
+            this.lblIdioma.TabIndex = 1;
+            this.lblIdioma.Text = "Idioma:";
             // 
-            // label3
+            // lblTema
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(439, 125);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(82, 29);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Tema:";
+            this.lblTema.AutoSize = true;
+            this.lblTema.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTema.Location = new System.Drawing.Point(439, 125);
+            this.lblTema.Name = "lblTema";
+            this.lblTema.Size = new System.Drawing.Size(82, 29);
+            this.lblTema.TabIndex = 2;
+            this.lblTema.Text = "Tema:";
             // 
-            // label5
+            // lblPrivacidad
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(181, 344);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(365, 29);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Politica de Privacidad de Datos...";
+            this.lblPrivacidad.AutoSize = true;
+            this.lblPrivacidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrivacidad.Location = new System.Drawing.Point(181, 344);
+            this.lblPrivacidad.Name = "lblPrivacidad";
+            this.lblPrivacidad.Size = new System.Drawing.Size(365, 29);
+            this.lblPrivacidad.TabIndex = 4;
+            this.lblPrivacidad.Text = "Politica de Privacidad de Datos...";
             // 
-            // label6
+            // lblEliminar
             // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(200, 226);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(221, 29);
-            this.label6.TabIndex = 5;
-            this.label6.Text = "Eliminar Mi Cuenta:";
+            this.lblEliminar.AutoSize = true;
+            this.lblEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEliminar.Location = new System.Drawing.Point(200, 226);
+            this.lblEliminar.Name = "lblEliminar";
+            this.lblEliminar.Size = new System.Drawing.Size(221, 29);
+            this.lblEliminar.TabIndex = 5;
+            this.lblEliminar.Text = "Eliminar Mi Cuenta:";
             // 
-            // label7
+            // lblDesactivar
             // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(176, 286);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(245, 29);
-            this.label7.TabIndex = 6;
-            this.label7.Text = "Desactivar Mi Cuenta:";
-            this.label7.Click += new System.EventHandler(this.label7_Click);
+            this.lblDesactivar.AutoSize = true;
+            this.lblDesactivar.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDesactivar.Location = new System.Drawing.Point(176, 286);
+            this.lblDesactivar.Name = "lblDesactivar";
+            this.lblDesactivar.Size = new System.Drawing.Size(245, 29);
+            this.lblDesactivar.TabIndex = 6;
+            this.lblDesactivar.Text = "Desactivar Mi Cuenta:";
             // 
-            // comboBox1
+            // cmbIdioma
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.cmbIdioma.FormattingEnabled = true;
+            this.cmbIdioma.Items.AddRange(new object[] {
             "Español",
             "Ingles"});
-            this.comboBox1.Location = new System.Drawing.Point(247, 130);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(161, 24);
-            this.comboBox1.TabIndex = 7;
+            this.cmbIdioma.Location = new System.Drawing.Point(247, 130);
+            this.cmbIdioma.Name = "cmbIdioma";
+            this.cmbIdioma.Size = new System.Drawing.Size(161, 24);
+            this.cmbIdioma.TabIndex = 7;
+            this.cmbIdioma.SelectedIndexChanged += new System.EventHandler(this.cmbIdioma_SelectedIndexChanged);
             // 
-            // comboBox2
+            // cmbTema
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Items.AddRange(new object[] {
+            this.cmbTema.FormattingEnabled = true;
+            this.cmbTema.Items.AddRange(new object[] {
             "Claro",
             "Oscuro"});
-            this.comboBox2.Location = new System.Drawing.Point(527, 132);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(161, 24);
-            this.comboBox2.TabIndex = 8;
+            this.cmbTema.Location = new System.Drawing.Point(527, 132);
+            this.cmbTema.Name = "cmbTema";
+            this.cmbTema.Size = new System.Drawing.Size(161, 24);
+            this.cmbTema.TabIndex = 8;
             // 
-            // button1
+            // btnEliminar
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(444, 226);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(123, 38);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "Eliminar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.Location = new System.Drawing.Point(444, 226);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(123, 38);
+            this.btnEliminar.TabIndex = 9;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnDesactivar
             // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(444, 285);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(123, 38);
-            this.button2.TabIndex = 10;
-            this.button2.Text = "Desactivar";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnDesactivar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDesactivar.Location = new System.Drawing.Point(444, 285);
+            this.btnDesactivar.Name = "btnDesactivar";
+            this.btnDesactivar.Size = new System.Drawing.Size(123, 38);
+            this.btnDesactivar.TabIndex = 10;
+            this.btnDesactivar.Text = "Desactivar";
+            this.btnDesactivar.UseVisualStyleBackColor = true;
             // 
-            // label4
+            // lbl2pasos
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(97, 185);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(324, 29);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Autentificación en dos pasos:";
-            this.label4.Click += new System.EventHandler(this.label4_Click);
+            this.lbl2pasos.AutoSize = true;
+            this.lbl2pasos.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2pasos.Location = new System.Drawing.Point(97, 179);
+            this.lbl2pasos.Name = "lbl2pasos";
+            this.lbl2pasos.Size = new System.Drawing.Size(324, 29);
+            this.lbl2pasos.TabIndex = 3;
+            this.lbl2pasos.Text = "Autentificación en dos pasos:";
             // 
             // FrmConfiguracion
             // 
@@ -161,19 +160,20 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnDesactivar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.cmbTema);
+            this.Controls.Add(this.cmbIdioma);
+            this.Controls.Add(this.lblDesactivar);
+            this.Controls.Add(this.lblEliminar);
+            this.Controls.Add(this.lblPrivacidad);
+            this.Controls.Add(this.lbl2pasos);
+            this.Controls.Add(this.lblTema);
+            this.Controls.Add(this.lblIdioma);
             this.Controls.Add(this.lblConfiguracionAvanzada);
             this.Name = "FrmConfiguracion";
             this.Text = "FrmConfiguracion";
+            this.Load += new System.EventHandler(this.FrmConfiguracion_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -182,15 +182,15 @@
         #endregion
 
         private System.Windows.Forms.Label lblConfiguracionAvanzada;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblIdioma;
+        private System.Windows.Forms.Label lblTema;
+        private System.Windows.Forms.Label lblPrivacidad;
+        private System.Windows.Forms.Label lblEliminar;
+        private System.Windows.Forms.Label lblDesactivar;
+        private System.Windows.Forms.ComboBox cmbIdioma;
+        private System.Windows.Forms.ComboBox cmbTema;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnDesactivar;
+        private System.Windows.Forms.Label lbl2pasos;
     }
 }
