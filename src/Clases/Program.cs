325f8494649488572147f2fb7ff536c426414cfb
IZANNAVARROPC\izann <izannavarro2005@gmail.com>
2024-05-16 16:59:09 +0200

    Formularios y clase estática para multilenguaje en proceso

--- src/Clases/Program.cs
+++ src/Clases/Program.cs
@@ -16,7 +16,7 @@ namespace piTest
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmConfiguracion());
+            Application.Run(new FrmChat());
         }
     }
 }
