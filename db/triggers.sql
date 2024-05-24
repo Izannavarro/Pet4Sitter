9d55849a2c5935f40c0c5a0ae60c657ddc5d9ae9
Rafael <65158117+rxvlc@users.noreply.github.com>
2024-05-24 10:09:12 +0200

    Update triggers.sql

--- db/triggers.sql
+++ db/triggers.sql
@@ -10,3 +10,8 @@ INSERT INTO user_logs (id_user, action, details)
     VALUES (NEW.id_user, 'UPDATE', CONCAT('Usuario actualizado con ID: ', NEW.id_user, ', Nombre: ', NEW.name));
 END
 
+##Trigger after delete usuarios:
+CREATE DEFINER=`root`@`%` TRIGGER `users_after_delete` AFTER DELETE ON `users` FOR EACH ROW BEGIN
+ INSERT INTO user_logs (id_user, action, details) 
+    VALUES (null, 'DELETE', CONCAT('Usuario eliminado con ID: ', OLD.id_user, ', Nombre: ', OLD.name));
+END
